fe32b560ae19532c8b61297a2ea5c73d6d1d3b64
Dsr2004 <davitdy2015@gmail.com>
2022-12-20 18:58:48 -0500

    merge del bobo de jhorman

--- La_Bonanza/settings.py
+++ La_Bonanza/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'La_Bonanza',
         'USER': 'postgres',
-        'PASSWORD': 'AdminLambda2022',
+        'PASSWORD': 'Admin2022',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
